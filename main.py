9f898309253a90202168634c8c9a1c8983da73d5
Alessandro <alessandrosoaresrangel1@gmail.com>
2024-04-26 17:09:12 -0300

    creating the Food class and detecting the collision to it

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import time
 from turtle import Screen
 from snake import Snake
+from food import Food
 
 
 screen = Screen()
 
-screen.setup(width=600,height=600)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Jogo da Cobrinha")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -22,6 +25,9 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 screen.exitonclick()
